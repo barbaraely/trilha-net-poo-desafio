e9dc3cd8ae7766f670bf4608eb798b560cc4f7f2
Bárbara Thaís Ely <barbarathaisely@gmail.com>
2023-11-05 11:44:47 -0300

    Desafio concluído!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main()
+    {
+        Nokia nokia = new Nokia("12345", "Nokia 3310", "123456789", 16);
+        Iphone iphone = new Iphone("67890", "iPhone 12", "987654321", 128);
+
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("WhatsApp");
+
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Facebook");
+    }
+}
